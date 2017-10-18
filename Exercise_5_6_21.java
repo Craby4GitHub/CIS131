4aeb441bd9abdba7d2b301349faf0d16f1cfdf36
wrcrabtree <wrcrabtree@EC-E513138626SC.pcc-domain.pima.edu>
2017-10-18 16:38:53 -0700

    Updated homework

--- Exercise_5_6_21.java
+++ Exercise_5_6_21.java
@@ -1,4 +1,21 @@
 
+/********************************************************************************************************************
+ * Lab2.java Author: Will Crabtree CIS 131: Programming and Problem Solving II
+ * 
+ * write a method that returns a number, given an uppercase letter
+ * 
+ * write a test program that prompts the user to enter a phone number as a
+ * string. The input number may contain letters. the program translates a
+ * letter(upper or lower) to a digit and leaves all other characters intact.
+ * 
+ * numbers like 
+ * 	1-800-flowers = 1-800-3569377
+ *  1800flowers = 18003569377
+ ********************************************************************************************************************/
+
 public class Exercise_5_6_21 {
 
+	public static void main(String[] args) {
+		// int getnumber(char uppercase)
+	}
 }
--- Exercise_5_6_5.java
+++ Exercise_5_6_5.java
@@ -1,4 +1,20 @@
 
-public class Exercise_5_6_5 {
+/********************************************************************************************************************
+  * Lab2.java
+  * Author: Will Crabtree
+  * CIS 131: Programming and Problem Solving II
+  * 
+  * write a method with the following header to display three numbers in increasing order
+  * 
+  * Write a test program that prompts the user to enter three numbers and invokes the method to display them in increasing order
+  ********************************************************************************************************************/
 
+public class Exercise_5_6_5 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void displaySortedNumbers(double num1, double num2, double num3){
+		
+	}
 }
